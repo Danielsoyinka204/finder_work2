ef882dcf7c2509a58b13a64321655943911b541b
waleedahmad36 <waleed09ahmad42@gmail.com>
2024-11-15 09:36:08 -0800

    completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+
+localStorage.removeItem('responses');
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
