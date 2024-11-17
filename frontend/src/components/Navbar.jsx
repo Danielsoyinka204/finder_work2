0f575acf2fc9594ee15efc12cf77af53d05575ef
waleedahmad36 <waleed09ahmad42@gmail.com>
2024-11-17 09:52:09 -0800

    working increadibily fine

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full  bg-gray-900 text-white py-6 px-4' >
+    <div className='w-full  bg-gray-900 text-white py-4 px-4' >
         <div className='max-w-5xl mx-auto flex items-center justify-between' >
-            <h3  className='text-3xl font-bold' >Finder</h3>
+            <h3  className='text-3xl font-bold' >
+              <img src='/header.jpeg' alt='logo' className='w-20 h-20 rounded-full' />
+            </h3>
         </div>
     </div>
   )
